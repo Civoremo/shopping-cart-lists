68ea4c65527a008d500eb2cabfe644b59d0afa06
unknown <civoremon@gmail.com>
2019-09-02 11:24:39 -0400

    Updates

---
+++ precache-manifest.72412be8c60ee719ec5be41f31538ee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e88b8e45114bd87913deb305506f2f",
+    "url": "/Civoremo/shopping-cart-lists/tree/master/shopping_list/index.html"
+  },
+  {
+    "revision": "3cdad325fc22ff4e009d",
+    "url": "/Civoremo/shopping-cart-lists/tree/master/shopping_list/static/css/main.f0179754.chunk.css"
+  },
+  {
+    "revision": "58e7a2540f1dbde85ab8",
+    "url": "/Civoremo/shopping-cart-lists/tree/master/shopping_list/static/js/2.58271266.chunk.js"
+  },
+  {
+    "revision": "3cdad325fc22ff4e009d",
+    "url": "/Civoremo/shopping-cart-lists/tree/master/shopping_list/static/js/main.b3b7b6d7.chunk.js"
+  },
+  {
+    "revision": "84708ac53c12f8966e6d",
+    "url": "/Civoremo/shopping-cart-lists/tree/master/shopping_list/static/js/runtime~main.ae9d455a.js"
+  }
+]);
